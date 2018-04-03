f1542f6f60901773beb6240a704ba90317a0c465
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-04-03 14:17:29 +0530

    Add files via upload

--- cryp15.py
+++ cryp15.py
@@ -1,8 +1,12 @@
-s=raw_input()
-#incomplete code correcions to be made
+import binascii
+
+s=binascii.unhexlify(raw_input())
 pad=ord(s[-1:])
 check=s[-pad:]
-if(check==s[:-1]*pad or pad<=16 or pad!=0):
+
+print(len(s))
+print(pad)
+if(ord(s[16-pad])==pad and (check)==(s[-1:]*pad) and pad<=16 and pad!=0 ):
 	print("cool")
 else:
 	print("error")
