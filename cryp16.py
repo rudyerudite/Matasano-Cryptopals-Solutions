45fd28383258c8375d0a5e3bd62415d611625857
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-04-19 18:23:28 +0530

    Add files via upload

--- cryp16.py
+++ cryp16.py
@@ -1,6 +1,6 @@
 from Crypto.Cipher import AES
 import os
-import binascii
+
 
  # ur random key generated for encryption in CBC mode
 key=os.urandom(16)
@@ -15,17 +15,19 @@ def pkcs(a,bs):
 #getting the encrypted text as str1+payload+str2
 def preapnd(instr):#appending and prepending the input with random strings and then paddding the resultant and encrypting it
 				   #this is supposed to be my supplied main encryption fn and I have to exploit it 
-	instr=instr.replace(';','$').replace('=','$')
+	instr=instr.replace(';','?').replace('=','?')
 	str1="comment1=cooking%20MCs;userdata="
 	str2=";comment2=!20like!20a%20pound!20of%20bacon"
-	#print(str1+instr+str2)
+	ash=str1+instr+str2
+	print(ash[32])
 	str1=obj.encrypt(pkcs(str1+instr+str2,16))
 	
 	return str1		#returns a string that has been encrypted and padded
 
 #check if you are the admin or not
 def check(_str):
-	_str=obj.decrypt(_str)
+	obj1=AES.new(key,AES.MODE_CBC,iv)
+	_str=obj1.decrypt(_str)
 	print(_str)
 	return ";admin=true;" in _str
 #take in my payload i.e. basically admin=true???
@@ -34,22 +36,18 @@ def exploit(inp):
 	#need to get the blocksize of the prefix so as to reach the crux
 	print("Welcome to slytherin")
 	ct=list(preapnd(inp))
+	f="".join(ct)
 	ctnw=""
-	#ct1=list(ct[32:48])
-	#k=""
-	#ct1[0]=chr((ord(ct1[0])^ord("$"))^ord(";"))
-	#ct1[6]=chr((ord(ct1[6])^ord("$"))^ord("="))
-	#ct1[11]=chr((ord(ct1[11])^ord("$"))^ord(";"))
-	#fg="".join(ct1)
-	#ctnw=ct[:32]+fg+ct[48:]
-	print(obj.decrypt(ct[32]))
-	ct[32]=chr((ord(ct[32])^ord("$"))^ord(";"))
-	ct[38]=chr((ord(ct[38])^ord("$"))^ord("="))
-	ct[43]=chr((ord(ct[43])^ord("$"))^ord(";"))
+	ct[16]=chr(ord(ct[16])^ord('?')^ord(';'))
+	ct[22]=chr(ord(ct[22])^ord('?')^ord('='))
+	ct[27]=chr(ord(ct[27])^ord('?')^ord(';'))
 	ctnw="".join(ct)
+	'''for i in range(len(f)) :
+		if(f[i]!=ctnw[i]):
+			print(ctnw[i],f[i])
+			print("Ron's stupidity!")'''																						
 	k=check(ctnw)
-	print(k)
-	if(k=="True"):
+	if(k==True):
 		print("Alohomora")
 	else:
 		print("I'm for Snape's detention tonight")
