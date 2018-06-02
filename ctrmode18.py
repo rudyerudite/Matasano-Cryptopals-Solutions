43e2a80352f455f5f75fc8ebf729b707f5bed520
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-06-02 16:58:12 +0530

    Update ctrmode18.py

--- ctrmode18.py
+++ ctrmode18.py
@@ -1,13 +1,15 @@
 from Crypto.Cipher import AES
 import Crypto.Util.Counter
 import struct
+
 ct=("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==").decode("base64")
 key="YELLOW SUBMARINE"
-nonce=0	#understand it is as nonce+intial counter
-nonce=struct.pack("<Q",nonce).encode("hex") #applying little endian format || o/wise arg gives error
-print (nonce)
-ctr=Crypto.Util.Counter.new(128, initial_value=nonce) #when not usinng this prompting error counter must be a callable object
-obj=AES.new(key,AES.MODE_CTR,counter=ctr) #counter keyword is necessary
-print(obj.decrypt(ct)) #not giving me out the  whole text correctly
+nonce=0
+count=0
+
+l=((struct.pack("<Q",nonce))) #nonce+count is used as a key stream together
+ltr=Crypto.Util.Counter.new(64, prefix=l, initial_value=count,little_endian=True) #rest of the 64 bits will belong to nonce this fn will be for count
+obj=AES.new(key,AES.MODE_CTR,counter=ltr) #counter keyword is necessary
+print(obj.decrypt(ct)) 
 
 
