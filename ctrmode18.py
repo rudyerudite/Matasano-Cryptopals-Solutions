d0f27602fac68f6c9c65cf4e64d7f01b28d4e565
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-06-02 11:53:35 +0530

    Add files via upload

---
+++ ctrmode18.py
@@ -0,0 +1,13 @@
+from Crypto.Cipher import AES
+import Crypto.Util.Counter
+import struct
+ct=("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==").decode("base64")
+key="YELLOW SUBMARINE"
+nonce=0	#understand it is as nonce+intial counter
+nonce=struct.pack("<Q",nonce).encode("hex") #applying little endian format || o/wise arg gives error
+print (nonce)
+ctr=Crypto.Util.Counter.new(128, initial_value=nonce) #when not usinng this prompting error counter must be a callable object
+obj=AES.new(key,AES.MODE_CTR,counter=ctr) #counter keyword is necessary
+print(obj.decrypt(ct)) #not giving me out the  whole text correctly
+
+
