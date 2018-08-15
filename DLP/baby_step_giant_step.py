f1f9f60e378b291e6f9d9cad1b7d404fc607da17
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-08-15 23:28:50 +0530

    Create baby_step_giant_step.py

---
+++ DLP/baby_step_giant_step.py
@@ -0,0 +1,22 @@
+#of the form b=a^x mod p
+#split x=i*m+j
+#g**im * g*j eqv to b mod p
+#g**im eqv to b*g^-j mod p
+import math
+from Crypto.Util.number import *
+
+a,b,p=map(int, raw_input().split())
+m=int(math.sqrt(p-1)+1)
+
+l=[]
+for i in range(0,m):#why is the limit m?
+	l.append(a**(i*m))#root m number of possibilities
+
+for j in range(0,m):
+	for k in range(m):
+		if((b*inverse(a**j,p))%p in l):
+			break
+	break
+x=j*m+k
+print(x)
+
