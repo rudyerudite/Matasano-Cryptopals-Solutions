34a9ac8a779d525603ee37720b8cbca4f871552d
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-08-16 14:25:54 +0530

    Update baby_step_giant_step.py

--- DLP/baby_step_giant_step.py
+++ DLP/baby_step_giant_step.py
@@ -10,13 +10,12 @@ m=int(math.sqrt(p-1)+1)
 
 l=[]
 for i in range(0,m):#why is the limit m?
-	l.append(pow(a,i*m,p)#root m number of possibilities
+	l.append(pow(a,i*m,p))#root m number of possibilities
 
 for j in range(0,m):
 	for k in range(m):
-		if(pow(b*inverse(pow(a,j),1,p)) in l):
+		if((pow(b*inverse(pow(a,j),p),1,p)) in l):
 			break
 	break
 x=j*m+k
 print(x)
-
